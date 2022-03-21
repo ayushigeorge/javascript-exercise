8d2e8856df6083508d4b9bb989b5fea7199a344a
ayushigeorge <ayushigeorge31@gmail.com>
2022-03-21 04:44:52 -0700

    Implementing Functions in JS with Proper formatting.

--- function.js
+++ function.js
@@ -1,20 +1,26 @@
 // Implementing  Functions 
-console.log("hello, world");
+console.log("Functions in JS");
 
 //Implemented Anonymous Function
-const add= function(a,b){
+const addition= function(a,b){
     return a+b;
 }
 
-const multi = function(a,b){
+const multiplication = function(a,b){
     return a*b;
 }
 
 // Implemented Normal Function
-function sub(){
+function subtraction(){
     return a-b;
 }
 //Implemented Arrow Function 
-const div = (a,b) => a/b;
+let a= 100;
+let b= 50;
+const division = (a,b) => a/b ;
 
-const mod = (a,b)=> a%b;
+
+
+let c = 9;
+let d= 90
+const mod = (c,d)=> d%c;
