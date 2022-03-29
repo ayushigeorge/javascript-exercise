254e7fa6ead8de924aaefc6a99d1655af4016f4a
ayushigeorge <ayushigeorge31@gmail.com>
2022-03-29 18:04:20 +0530

    Merge branch 'ayushig/callby.js-rb' into ayushig/variables-in-js

---
+++ destructuring.js
@@ -0,0 +1,20 @@
+// when destructuring array, we use the index operator.
+let array = [1, 2, 3, 4, 5];
+let first, second;
+
+[first, second] = array;
+console.log(first, second);
+
+// to extract the random value in given array
+
+let 
+
+// to extract output from string of array
+
+array2 = ["apple", "banana", "cherry"];
+[1,2,3]= array2;
+console.log(1);
+console.log(2);
+console.log(3);
+
+
